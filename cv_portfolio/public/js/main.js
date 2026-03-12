ec16d3571c4a57d116479194cda00db8cf278f09
Muhammad Islam <islamjutt56.i@gmail.com>
2026-03-12 23:13:18 +0500

    Add files via upload

---
+++ cv_portfolio/public/js/main.js
@@ -0,0 +1,57 @@
+const elements=document.querySelectorAll(
+".skill-card,.service-card,.project-card"
+);
+
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+elements.forEach(el=>{
+observer.observe(el);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+const projectItems = document.querySelectorAll(".project-item");
+
+filterButtons.forEach(button => {
+
+button.addEventListener("click", () => {
+
+document.querySelector(".filter-btn.active").classList.remove("active");
+button.classList.add("active");
+
+const filter = button.getAttribute("data-filter");
+
+projectItems.forEach(item => {
+
+if (filter === "all") {
+item.style.display = "block";
+}
+
+else if (item.classList.contains(filter)) {
+item.style.display = "block";
+}
+
+else {
+item.style.display = "none";
+}
+
+});
+
+});
+
+});
+
+});
